726994c57a97e3d0c35c2f4445a59673b18ca92a
wayangalihpratama <wgprtm@gmail.com>
2021-06-14 22:07:00 +0800

    [#2] Force migration

--- ci/deploy-semaphore.sh
+++ ci/deploy-semaphore.sh
@@ -76,7 +76,7 @@ ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
     -p 18765 \
     -o UserKnownHostsFile=/dev/null \
     -o StrictHostKeyChecking=no \
-    u7-nnfq7m4dqfyx@35.214.170.100 "cd www/tc.akvo.org/public_html/${FOLDER}/ && /usr/local/bin/php73 artisan migrate"
+    u7-nnfq7m4dqfyx@35.214.170.100 "cd www/tc.akvo.org/public_html/${FOLDER}/ && /usr/local/bin/php73 artisan migrate --force"
 
 echo "===================================================="
 echo "Done deploying ${FOLDER}"
