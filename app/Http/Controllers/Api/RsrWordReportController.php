0c11aa0da42bc5273c6c2d74d199c55ab2cb52cd
wayangalihpratama <wgprtm@gmail.com>
2021-06-17 09:56:11 +0800

    [#13] Refine function to generate table on word doc

--- app/Http/Controllers/Api/RsrWordReportController.php
+++ app/Http/Controllers/Api/RsrWordReportController.php
@@ -8,12 +8,17 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 use App\Partnership;
-use PhpOffice\PhpWord\Shared\Converter;
-use PhpOffice\PhpWord\Style\TablePosition;
 use App\Http\Controllers\Api\ChartController;
+use PhpOffice\PhpWord\SimpleType\Jc;
+use PhpOffice\PhpWord\Style\Table;
 
 class RsrWordReportController extends Controller
 {
+    private $alignHCentered = array('alignment' => Jc::CENTER);
+    private $alignVCentered = array('valign' => 'center');
+    private $alignJustify = array('alignment' => Jc::BOTH);
+    private $alignRight = array('align' => Jc::END);
+
     public function getRsrWordReport(Request $request)
     {
         $chart = new ChartController();
@@ -71,48 +76,98 @@ class RsrWordReportController extends Controller
                 )
             )
         );
+        $titleStyle = array('size' => 12, 'bold' => true);
         $listItemStyle = array('bold' => true);
-        $justify = array('alignment' => \PhpOffice\PhpWord\SimpleType\Jc::BOTH);
-        $cellHCentered = array('alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER);
-        $cellVCentered = array('valign' => 'center');
-        $fancyTableStyle = array('borderSize' => 6, 'borderColor' => '999999', 'layout' => \PhpOffice\PhpWord\Style\Table::LAYOUT_AUTO);
         $lineStyle = array('weight' => 1, 'width' => 430, 'height' => 0);
         // EOL Style
 
-        $section->addText('Quarterly report month-month 2020', array('size' => 12, 'bold' => true), $cellHCentered);
-        $section->addText($country, array('size' => 12, 'bold' => true), $cellHCentered);
+        $section->addText('Quarterly report month-month 2020', $titleStyle, $this->alignHCentered);
+        $section->addText($country, $titleStyle, $this->alignHCentered);
         $section->addTextBreak(2);
 
-        $section->addText('Partnership Name: '.$data['project'], array('size' => 12, 'bold' => true));
+        $section->addText('Partnership Name: '.$data['project'], $titleStyle);
         $section->addLine($lineStyle);
 
         // Start table rendering
         $section->addListItem('Summary of the PPPs contribution to the UIIs', 1, $listItemStyle, 'multilevel-'.$n);
+        $phpWord = $this->renderTable($phpWord, $section, $data, $columns);
+        // EOL of Table
+
+        $section->addTextBreak(2);
+        $section->addListItem('Incubating inclusive model', 1, $listItemStyle, 'multilevel-'.$n);
+        $section->addTextBreak(1);
+        $section->addListItem('Govern and adopt inclusive agribusiness partnership', 2, $listItemStyle, 'multilevel-'.$n);
+        $section->addListItem('Improve access to nutritional food for the BoP consumer', 2, $listItemStyle, 'multilevel-'.$n);
+        $section->addListItem('Foster competitiveness and inclusiveness of the food value chain', 2, $listItemStyle, 'multilevel-'.$n);
+        $section->addListItem('Professionalize Agribusiness Clusters', 2, $listItemStyle, 'multilevel-'.$n);
+        $section->addListItem('Strengthen the enabling agribusiness environment', 2, $listItemStyle, 'multilevel-'.$n);
+
+        $section->addTextBreak(2);
+        $section->addListItem('Other activities', 0, $listItemStyle, 'multilevel-'.$n);
+        $section->addTextBreak(1);
+        $section->addListItem('Action research', 1, $listItemStyle, 'multilevel-'.$n);
+        $section->addListItem('Monitoring and Evaluation', 1, $listItemStyle, 'multilevel-'.$n);
+        $section->addListItem('Communications', 1, $listItemStyle, 'multilevel-'.$n);
+
+        $section->addTextBreak(2);
+        $section->addListItem('Conclusion and follow-up', 0, $listItemStyle, 'multilevel-'.$n);
+
+        $footer = $section->addFooter();
+        $footer->addPreserveText('Page {PAGE} of {NUMPAGES}', null, $this->alignRight);
+
+        return $phpWord;
+    }
+
+    private function renderTable($phpWord, $section, $data, $columns)
+    {
+        $fancyTableStyle = array('borderSize' => 6, 'borderColor' => '999999', 'layout' => Table::LAYOUT_AUTO);
         $spanTableStyleName = 'Rsr Table';
         $phpWord->addTableStyle($spanTableStyleName, $fancyTableStyle);
         $table = $section->addTable($spanTableStyleName);
 
         // Header row
         $table->addRow();
-        $columns->each(function ($col) use ($table, $cellHCentered, $cellVCentered) {
-            if (count($col['subtitle']) === 0) {
-                $cellRowSpan = array('vMerge' => 'restart', 'valign' => 'center');
-                $table->addCell(350, $cellRowSpan)->addText($col['uii'], $cellHCentered);
+        $phpWord = $this->renderTableHeader($phpWord, $table, $columns);
+        $table->addRow();
+        $phpWord = $this->renderTableHeader($phpWord, $table, $columns, false);
+        // end of header row
+
+        // Body
+        $table->addRow();
+        $data['columns']->each(function ($col) use ($table) {
+            if (count($col['rsr_dimensions']) === 0) {
+                $table->addCell(1000)->addText($col['total_actual_value'], $this->alignHCentered);
             }
-            if (count($col['subtitle']) > 0) {
-                $subtitles = collect($col['subtitle'])->pluck('values')->flatten(1);
-                $cellColSpan = array('gridSpan' => count($subtitles), 'valign' => 'center');
-                $table->addCell(count($subtitles) * 350, $cellColSpan)->addText($col['uii'], null, $cellHCentered);
+            if (count($col['rsr_dimensions']) > 0) {
+                $dimensions = collect($col['rsr_dimensions'])->pluck('rsr_dimension_values')->flatten(1);
+                foreach ($dimensions as $key => $value) {
+                    $table->addCell(700, $this->alignVCentered)->addText($value['total_actual_value'], null, $this->alignHCentered);
+                }
             }
         });
+        // end of body
+        return $phpWord;
+    }
 
-        $table->addRow();
-        $columns->each(function ($col) use ($table, $cellHCentered, $cellVCentered) {
-            if (count($col['subtitle']) === 0) {
+    private function renderTableHeader($phpWord, $table, $columns, $firstRow=true)
+    {
+        $columns->each(function ($col) use ($table, $firstRow) {
+            // for first row
+            if (count($col['subtitle']) === 0 && $firstRow) {
+                $cellRowSpan = array('vMerge' => 'restart', 'valign' => 'center');
+                $table->addCell(350, $cellRowSpan)->addText($col['uii'], $this->alignHCentered);
+            }
+            if (count($col['subtitle']) > 0 && $firstRow) {
+                $subtitles = collect($col['subtitle'])->pluck('values')->flatten(1);
+                $cellColSpan = array('gridSpan' => count($subtitles), 'valign' => 'center');
+                $table->addCell(count($subtitles) * 350, $cellColSpan)->addText($col['uii'], null, $this->alignHCentered);
+            }
+            // for second row
+            if (count($col['subtitle']) === 0 && !$firstRow) {
                 $cellRowContinue = array('vMerge' => 'continue');
                 $table->addCell(350, $cellRowContinue);
             }
-            if (count($col['subtitle']) > 0) {
+            if (count($col['subtitle']) > 0 && !$firstRow) {
                 $subtitles = collect($col['subtitle'])->pluck('values')->flatten(1)->values();
                 foreach ($subtitles as $key => $value) {
                     $name = $value;
@@ -138,56 +193,15 @@ class RsrWordReportController extends Controller
                             $name = "JF";
                         }
                     }
-                    $table->addCell(350, $cellVCentered)->addText($name, null, $cellHCentered);
+                    $table->addCell(350, $this->alignVCentered)->addText($name, null, $this->alignHCentered);
                 }
             }
         });
-        // end of header row
-
-        // Body
-        $table->addRow();
-        $data['columns']->each(function ($col) use ($table, $cellHCentered, $cellVCentered) {
-            if (count($col['rsr_dimensions']) === 0) {
-                $table->addCell(1000)->addText($col['total_actual_value'], $cellHCentered);
-            }
-            if (count($col['rsr_dimensions']) > 0) {
-                $dimensions = collect($col['rsr_dimensions'])->pluck('rsr_dimension_values')->flatten(1);
-                foreach ($dimensions as $key => $value) {
-                    $table->addCell(700, $cellVCentered)->addText($value['total_actual_value'], null, $cellHCentered);
-                }
-            }
-        });
-        // end of body
-        // EOL of Table
-        $lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut feugiat tortor. Nullam risus felis, ultrices et feugiat a, elementum a odio. Nam tempor, sapien sit amet iaculis commodo, nibh diam venenatis dolor, et semper ante lorem et nibh. Praesent vitae velit sed eros sollicitudin accumsan sit amet eget magna. Ut malesuada ante eu arcu sollicitudin fermentum. Nam vulputate, lectus tempor accumsan placerat, velit libero posuere purus, sed tempus enim nisi ac lorem. Donec ornare justo elit, a vestibulum ipsum consequat id.';
-
-        $section->addTextBreak(2);
-        $section->addListItem('Incubating inclusive model', 1, $listItemStyle, 'multilevel-'.$n);
-        $section->addText($lorem, null, $justify);
-        $section->addTextBreak(1);
-        $section->addListItem('Govern and adopt inclusive agribusiness partnership', 2, $listItemStyle, 'multilevel-'.$n);
-        $section->addListItem('Improve access to nutritional food for the BoP consumer', 2, $listItemStyle, 'multilevel-'.$n);
-        $section->addListItem('Foster competitiveness and inclusiveness of the food value chain', 2, $listItemStyle, 'multilevel-'.$n);
-        $section->addListItem('Professionalize Agribusiness Clusters', 2, $listItemStyle, 'multilevel-'.$n);
-        $section->addListItem('Strengthen the enabling agribusiness environment', 2, $listItemStyle, 'multilevel-'.$n);
-
-        $section->addTextBreak(2);
-        $section->addListItem('Other activities', 0, $listItemStyle, 'multilevel-'.$n);
-        $section->addTextBreak(1);
-        $section->addListItem('Action research', 1, $listItemStyle, 'multilevel-'.$n);
-        $section->addListItem('Monitoring and Evaluation', 1, $listItemStyle, 'multilevel-'.$n);
-        $section->addListItem('Communications', 1, $listItemStyle, 'multilevel-'.$n);
-
-        $section->addTextBreak(2);
-        $section->addListItem('Conclusion and follow-up', 0, $listItemStyle, 'multilevel-'.$n);
-
-        $footer = $section->addFooter();
-        $footer->addPreserveText('Page {PAGE} of {NUMPAGES}', null, array('align' => \PhpOffice\PhpWord\SimpleType\Jc::END));
-
         return $phpWord;
     }
 
-    private function getPartnershipCache() {
+    private function getPartnershipCache()
+    {
         $partnership = Cache::get('partnership');
         if (!$partnership) {
             $partnership = Partnership::all();
