97d7df57537d354ca4de6405c8f6a8b5a0a79c2b
dedenbangkit <mail@dedenbangkit.com>
2021-10-28 19:48:00 +0700

    [#51] Move stores to IndexedDB with 2 hours expiration

--- resources/js/dexie.js
+++ resources/js/dexie.js
@@ -1,24 +1,24 @@
-import Dexie from 'dexie';
+import Dexie from "dexie";
 
 const defineDB = () => {
-    const db = new Dexie('2scale');
+    const db = new Dexie("2scale");
     db.version(1).stores({
-        databases: 'name,data',
-        charts: '++id,endpoint,filter,data'
+        databases: "name,data",
+        charts: "++id,endpoint,filter,data",
     });
     return db;
 };
 export const db = defineDB();
 
-export const storeDB = ({table, data, key}) => {
+export const storeDB = ({ table, data, key }) => {
     db.open();
-    db.transaction('rw', table, async () => {
+    db.transaction("rw", table, async () => {
         // Let's add some data to db:
         const res = await table.get(key);
         if (res === undefined) {
             await table.add(data);
         }
-    }).catch(function(err) {
+    }).catch(function (err) {
         console.error(err.stack || err);
     });
     return;
