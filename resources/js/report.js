f3aec765e860dc0ebe6c0031bbbfc0205bd011ed
wayangalihpratama <wgprtm@gmail.com>
2021-11-22 19:50:47 +0800

    [#61] Remove console log & prettier

--- resources/js/report.js
+++ resources/js/report.js
@@ -5,27 +5,37 @@ import { targetAndLastSync } from "./util";
 import { renderRsrTableTemplate, renderRsrTable } from "./rsrDatatables.js";
 
 const baseurl = $("meta[name=path]").attr("content");
-console.log(baseurl);
 
 const renderReportForm = () => {
     axios
         .get(baseurl + "/api/flow/partnerships")
-        .then(res => {
-            res.data.forEach(c => {
-                $("#country-level").append('<option value="'+c.id+'">'+c.name+'</option>');
+        .then((res) => {
+            res.data.forEach((c) => {
+                $("#country-level").append(
+                    '<option value="' + c.id + '">' + c.name + "</option>"
+                );
                 if (c.childrens.length > 0) {
-                    c.childrens.forEach(p => {
-                        $("#partnership-level").append('<option class="partnerships ppp-'+c.id+'" value="'+p.id+'">'+p.name+'</option>');
+                    c.childrens.forEach((p) => {
+                        $("#partnership-level").append(
+                            '<option class="partnerships ppp-' +
+                                c.id +
+                                '" value="' +
+                                p.id +
+                                '">' +
+                                p.name +
+                                "</option>"
+                        );
                     });
-                };
+                }
             });
             return res.data;
-        }).then(res => {
-            $(".partnerships").hide('fast');
+        })
+        .then((res) => {
+            $(".partnerships").hide("fast");
         });
-}
+};
 
-targetAndLastSync().then(el => {
+targetAndLastSync().then((el) => {
     $("#last-sync-temp").append(el);
 });
 
@@ -35,50 +45,76 @@ $("main").append(
             <div class="col-md-12">
                 <div class="card">
                     <div id="last-sync-temp"></div>
-                    <div class="card-header"><h3>Generate Internal Report</h3></div>
+                    <div class="card-header">
+                        <h3>Generate Internal Report</h3>
+                    </div>
                     <div class="card-body">
-                        <div class="d-flex justify-content-center align-items-center" id="loader-test">
-                          <form class="row form-inline">
-                            <div class="col-auto form-group required">
-                              <label class="control-label"></label>
-                              <select id="country-level" class="form-control">
-                                <option value="0" selected>Select Country (CTL)</option>
-                              </select>
-                            </div>
-                            <div  class="col-auto form-group">
-                              <label class="control-label"></label>
-                              <select id="partnership-level" class="form-control">
-                                <option value="0" selected>Select Partnership (PF)</option>
-                              </select>
-                            </div>
-                            <div class="col-auto form-group required">
-                              <label class="control-label"></label>
-                              <select id="year" class="form-control">
-                                <option value="0" selected>Year</option>
-                                <option value="2021">2021</option>
-                                <option value="2022">2022</option>
-                                <option value="2023">2023</option>
-                              </select>
-                            </div>
-                            <div class="col-auto form-group required">
-                              <label class="control-label"></label>
-                              <select id="selector" class="form-control">
-                                <option value="0" selected>Report Selector</option>
-                                <option value="1">Report 1</option>
-                                <option value="2">Report 2</option>
-                                <option value="3">Report 3</option>
-                              </select>
-                            </div>
-                            <div class="col-auto">
-                              <button id="generate-word-report" type="button" class="btn btn-sm btn-primary" style="height:35px;">Download</button>
-                            </div>
-                          </form>
+                        <div
+                            class="d-flex justify-content-center align-items-center"
+                            id="loader-test"
+                        >
+                            <form class="row form-inline">
+                                <div class="col-auto form-group required">
+                                    <label class="control-label"></label>
+                                    <select
+                                        id="country-level"
+                                        class="form-control"
+                                    >
+                                        <option value="0" selected>
+                                            Select Country (CTL)
+                                        </option>
+                                    </select>
+                                </div>
+                                <div class="col-auto form-group">
+                                    <label class="control-label"></label>
+                                    <select
+                                        id="partnership-level"
+                                        class="form-control"
+                                    >
+                                        <option value="0" selected>
+                                            Select Partnership (PF)
+                                        </option>
+                                    </select>
+                                </div>
+                                <div class="col-auto form-group required">
+                                    <label class="control-label"></label>
+                                    <select id="year" class="form-control">
+                                        <option value="0" selected>
+                                            Year
+                                        </option>
+                                        <option value="2021">2021</option>
+                                        <option value="2022">2022</option>
+                                        <option value="2023">2023</option>
+                                    </select>
+                                </div>
+                                <div class="col-auto form-group required">
+                                    <label class="control-label"></label>
+                                    <select id="selector" class="form-control">
+                                        <option value="0" selected>
+                                            Report Selector
+                                        </option>
+                                        <option value="1">Report 1</option>
+                                        <option value="2">Report 2</option>
+                                        <option value="3">Report 3</option>
+                                    </select>
+                                </div>
+                                <div class="col-auto">
+                                    <button
+                                        id="generate-word-report"
+                                        type="button"
+                                        class="btn btn-sm btn-primary"
+                                        style="height:35px;"
+                                    >
+                                        Download
+                                    </button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <hr/>
+        <hr />
     </div>
 );
 renderReportForm();
@@ -91,28 +127,33 @@ getCharts("report/total-activities", "second-row", "12");
 // renderRsrTableTemplate("datatables", "90%");
 // renderRsrTable(["0", "0"].join("/"), baseurl, "datatables");
 
-
 $("#country-level").on("change", () => {
     let country_id = $("#country-level").val();
     $("#partnership-level").val(0);
-    $(".partnerships").hide('fast');
-    $(".ppp-"+country_id).show('fast');
+    $(".partnerships").hide("fast");
+    $(".ppp-" + country_id).show("fast");
 });
 
 const showModalError = (response) => {
     $("#myModalAuthTitle").html("Error");
     $("#myModalAuthBody").html(
-        <div class="alert alert-warning" role="alert">{response}</div>
+        <div class="alert alert-warning" role="alert">
+            {response}
+        </div>
     );
     $("#myModalAuth").modal({ backdrop: "static", keyboard: false });
-}
+};
 
 $("#generate-word-report").on("click", () => {
     let country_id = $("#country-level").val();
     let partnership_id = $("#partnership-level").val();
     let year = $("#year").val();
     let selector = $("#selector").val();
-    if (Number(country_id) === 0 || Number(year) === 0 || Number(selector) === 0) {
+    if (
+        Number(country_id) === 0 ||
+        Number(year) === 0 ||
+        Number(selector) === 0
+    ) {
         showModalError("Country, Year, and Report Selector are required.");
         return;
     }
@@ -121,36 +162,53 @@ $("#generate-word-report").on("click", () => {
     $("#myModalAuthTitle").html("Please Wait");
     $("#myModalAuthBody").html(
         <div>
-            <br/>
+            <br />
             <div class="d-flex justify-content-center" id="loader-spinner">
-                <div class="spinner-border text-primary loader-spinner" role="status">
+                <div
+                    class="spinner-border text-primary loader-spinner"
+                    role="status"
+                >
                     <span class="sr-only">Loading...</span>
                 </div>
             </div>
-            <br/>
+            <br />
         </div>
-
     );
     $("#myModalAuth").modal({ backdrop: "static", keyboard: false });
 
     axios
-        .get(baseurl + "/api/rsr/word-report/" + country_id + "/" + partnership_id + "/" + year + "/" + selector)
-        .then(res => {
+        .get(
+            baseurl +
+                "/api/rsr/word-report/" +
+                country_id +
+                "/" +
+                partnership_id +
+                "/" +
+                year +
+                "/" +
+                selector
+        )
+        .then((res) => {
             $("#loader-spinner").remove();
             $("#myModalAuthTitle").html("Report ready to download");
             $("#myModalAuthBody").html(
                 <a target="_blank" href={res.data.link}>
-                    <button type="button" class="btn btn-primary"> Download Report</button>
+                    <button type="button" class="btn btn-primary">
+                        {" "}
+                        Download Report
+                    </button>
                 </a>
             );
             $("#myModalBtnClose").show();
         })
-        .catch(err => {
+        .catch((err) => {
             console.error(err);
             $("#loader-spinner").remove();
             $("#myModalAuthTitle").html("Error");
             $("#myModalAuthBody").html(
-                <div class="alert alert-danger" role="alert">{err.response.data}</div>
+                <div class="alert alert-danger" role="alert">
+                    {err.response.data}
+                </div>
             );
             $("#myModalBtnClose").show();
         });
