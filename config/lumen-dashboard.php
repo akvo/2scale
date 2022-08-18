699ab12241f51ad66fd07a685c967ce77b071327
wayangalihpratama <wgprtm@gmail.com>
2022-08-18 11:53:21 +0800

    [#93] Update lumen tab dropdown text

--- config/lumen-dashboard.php
+++ config/lumen-dashboard.php
@@ -2,35 +2,35 @@
 
 return [
   [
-    "label" => "UII-1",
+    "label" => "UII1-BoP",
     "link" => "https://2scale.akvolumen.org/s/nXF-v96KRF4"
   ],
   [
-    "label" => "UII-2",
+    "label" => "UII2-SHF",
     "link" => "https://2scale.akvolumen.org/s/8J1bSw8IW9g"
   ],
   [
-    "label" => "UII-3",
+    "label" => "UII3-EEP",
     "link" => "https://2scale.akvolumen.org/s/cc5z5V9pHxQ"
   ],
   [
-    "label" => "UII-4",
+    "label" => "UII4-SME",
     "link" => "https://2scale.akvolumen.org/s/QFX_0qixuXQ"
   ],
   [
-    "label" => "UII-5",
+    "label" => "UII5-NonFE",
     "link" => "https://2scale.akvolumen.org/s/p-d507870kw"
   ],
   [
-    "label" => "UII-6",
+    "label" => "UII6-MSME",
     "link" => "https://2scale.akvolumen.org/s/5_5fabJ5obA"
   ],
   [
-    "label" => "UII-7",
+    "label" => "UII7-INNO",
     "link" => "https://2scale.akvolumen.org/s/UevggdhAzvQ"
   ],
   [
-    "label" => "UII-8",
+    "label" => "UII8-FSERV",
     "link" => "https://2scale.akvolumen.org/s/IIPJcwIMimE"
   ],
 ];
